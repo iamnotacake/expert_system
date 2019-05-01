655f59bb0a1e5ff509e35d4cc3f69cdd8acda4a3
Anatoliy Lischynsky <alischyn@student.unit.ua>
2019-05-01 16:19:51 +0300

    Impl Display for Fact

--- src/fact.rs
+++ src/fact.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 pub enum Fact {
     Char(char),
@@ -7,4 +9,22 @@ pub enum Fact {
     Xor(Box<Fact>, Box<Fact>),
     IfThen(Box<Fact>, Box<Fact>),
     IfAndOnlyIf(Box<Fact>, Box<Fact>),
+}
+
+impl fmt::Display for Fact {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Fact::*;
+
+        match self {
+            Char(ref c) => write!(f, "{}", c)?,
+            Not(ref l) => write!(f, "!({})", l)?,
+            And(ref l, ref r) => write!(f, "({} + {})", l, r)?,
+            Or(ref l, ref r) => write!(f, "({} | {})", l, r)?,
+            Xor(ref l, ref r) => write!(f, "({} ^ {})", l, r)?,
+            IfThen(ref l, ref r) => write!(f, "{} => {}", l, r)?,
+            IfAndOnlyIf(ref l, ref r) => write!(f, "{} <=> {}", l, r)?,
+        }
+
+        Ok(())
+    }
 }
