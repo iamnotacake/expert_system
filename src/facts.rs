e5682e9aad0c1efaff4f18e274eb7c24b236b422
Anatoliy Lischynsky <alischyn@student.unit.ua>
2019-05-01 16:03:03 +0300

    Move Facts to separate module

---
+++ src/facts.rs
@@ -0,0 +1,47 @@
+use std::fmt;
+
+pub struct Facts {
+    yes: [bool; 26],
+    no: [bool; 26],
+}
+
+impl Facts {
+    pub fn new(chars: &str) -> Facts {
+        let mut yes = [false; 26];
+        let mut no = [false; 26];
+
+        for c in chars.chars() {
+            yes[(c as usize) - ('A' as usize)] = true;
+        }
+
+        Facts { yes, no }
+    }
+}
+
+impl fmt::Debug for Facts {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Facts {{ true: ")?;
+
+        for (idx, &val) in self.yes.iter().enumerate() {
+            if val {
+                write!(f, "{}", (idx + 'A' as usize) as u8 as char)?;
+            } else {
+                write!(f, ".")?;
+            }
+        }
+
+        write!(f, " false: ")?;
+
+        for (idx, &val) in self.no.iter().enumerate() {
+            if val {
+                write!(f, "{}", (idx + 'A' as usize) as u8 as char)?;
+            } else {
+                write!(f, ".")?;
+            }
+        }
+
+        write!(f, " }}")?;
+
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@
 
 use std::fmt;
 
+pub mod facts;
+pub use facts::Facts;
+
 pub mod parser {
     include!(concat!(env!("OUT_DIR"), "/parser.rs"));
 }
@@ -22,50 +25,4 @@ pub enum Fact {
     Xor(Box<Fact>, Box<Fact>),
     IfThen(Box<Fact>, Box<Fact>),
     IfAndOnlyIf(Box<Fact>, Box<Fact>),
-}
-
-pub struct Facts {
-    yes: [bool; 26],
-    no: [bool; 26],
-}
-
-impl Facts {
-    pub fn new(chars: &str) -> Facts {
-        let mut yes = [false; 26];
-        let mut no = [false; 26];
-
-        for c in chars.chars() {
-            yes[(c as usize) - ('A' as usize)] = true;
-        }
-
-        Facts { yes, no }
-    }
-}
-
-impl fmt::Debug for Facts {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Facts {{ true: ")?;
-
-        for (idx, &val) in self.yes.iter().enumerate() {
-            if val {
-                write!(f, "{}", (idx + 'A' as usize) as u8 as char)?;
-            } else {
-                write!(f, ".")?;
-            }
-        }
-
-        write!(f, " false: ")?;
-
-        for (idx, &val) in self.no.iter().enumerate() {
-            if val {
-                write!(f, "{}", (idx + 'A' as usize) as u8 as char)?;
-            } else {
-                write!(f, ".")?;
-            }
-        }
-
-        write!(f, " }}")?;
-
-        Ok(())
-    }
 }
