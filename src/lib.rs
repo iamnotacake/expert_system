530c04d50a53322cae21257772b8451f06ce42b1
Anatoliy Lischynsky <alischyn@student.unit.ua>
2019-05-01 15:58:39 +0300

    Split facts into 'true' and 'false' groups

--- src/lib.rs
+++ src/lib.rs
@@ -25,26 +25,38 @@ pub enum Fact {
 }
 
 pub struct Facts {
-    arr: [bool; 26],
+    yes: [bool; 26],
+    no: [bool; 26],
 }
 
 impl Facts {
     pub fn new(chars: &str) -> Facts {
-        let mut arr = [false; 26];
+        let mut yes = [false; 26];
+        let mut no = [false; 26];
 
         for c in chars.chars() {
-            arr[(c as usize) - ('A' as usize)] = true;
+            yes[(c as usize) - ('A' as usize)] = true;
         }
 
-        Facts { arr }
+        Facts { yes, no }
     }
 }
 
 impl fmt::Debug for Facts {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Facts {{ ")?;
+        write!(f, "Facts {{ true: ")?;
 
-        for (idx, &val) in self.arr.iter().enumerate() {
+        for (idx, &val) in self.yes.iter().enumerate() {
+            if val {
+                write!(f, "{}", (idx + 'A' as usize) as u8 as char)?;
+            } else {
+                write!(f, ".")?;
+            }
+        }
+
+        write!(f, " false: ")?;
+
+        for (idx, &val) in self.no.iter().enumerate() {
             if val {
                 write!(f, "{}", (idx + 'A' as usize) as u8 as char)?;
             } else {
